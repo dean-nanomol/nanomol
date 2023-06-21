0b051a241e5b3f135825bba8df8c11359d982474
dean-nanomol <135820523+dean-nanomol@users.noreply.github.com>
2023-06-21 12:12:56 +0200

    update hdf5 viewer

--- utils/hdf5_viewer.py
+++ utils/hdf5_viewer.py
@@ -17,25 +17,59 @@ class hdf5_viewer(QtWidgets.QWidget):
         self.datafile = datafile
         uic.loadUi(r'hdf5_viewer.ui', self)
         self.initialise_plot()
-        self.build_datafile_tree()
+        self.load_datafile_tree()
+        self.datafile_treeview_Y.selectionModel().selectionChanged.connect(self.update_plot)
         
     def initialise_plot(self):
         self.data_plot = pg.PlotWidget()
         self.plot_grid.replaceWidget(self.plot_widget, self.data_plot)
         self.data_plot.setBackground('w')
         
-    def build_datafile_tree(self):
+    def load_datafile_tree(self):
+        """ build QTreeView from hdf5 dataset. The name of each dataset or group is a QStandardItem """
         self.datafile_tree_model = QStandardItemModel()
-        self.datafile_treeview.setModel(self.datafile_tree_model)
-        self.treeview_root = self.datafile_tree_model.invisibleRootItem()
-        self.build_datafile_branch(self.datafile, self.treeview_root)
+        self.datafile_treeview_X.setModel(self.datafile_tree_model)
+        self.treeview_root = self.datafile_tree_model.invisibleRootItem()  # get root item
+        self.load_datafile_branch(self.datafile, self.treeview_root)
+        self.datafile_treeview_Y.setModel(self.datafile_treeview_X.model()) # copy tree for Y axis
         
-    def build_datafile_branch(self, datafile_group, tree_item):
+    def load_datafile_branch(self, datafile_group, tree_item):
+        """ populate branch with datasets and recursively build sub-branches """
         for row_counter, key in enumerate(datafile_group.keys()):
             tree_item.insertRow(row_counter, QStandardItem(key))
             if isinstance(datafile_group[key], h5py.Group):
-                """ recursively build a branch from each group """
-                self.build_datafile_branch(datafile_group[key], tree_item.child(row_counter))
+                """ recursively build a tree branch for each datafile group """
+                self.load_datafile_branch(datafile_group[key], tree_item.child(row_counter))
+        
+    #def load_attributes(self)
+                
+    def get_attributes(self, datafile_item):
+        """
+        Returns python dictionary of attributes
+        
+        datafile_item: h5py group or dataset        
+        """
+        if isinstance(datafile_item, (h5py.Group, h5py.Dataset) ):
+            attributes = {}
+            for attr_name, attr_value in datafile_item.attrs.items():
+                attributes[attr_name] = attr_value
+            return attributes
+    
+    # def update_plot(self, a, b):
+    #     print(a)
+    #     print(b)
+    #     sender = self.sender()
+    #     print('updated')
+    #     print(sender.model())
+    #     print(sender.currentIndex())
+    #     #print(sender.model().item(sender.currentIndex()) )
+    #     if sender == self.datafile_treeview_Y.selectionModel:
+    #         print('type match')
+    
+    def update_plot(self):
+        selected_indices = self.datafile_treeview_Y.selectionModel().selectedIndexes()
+        for index in selection:
+            print(self.datafile_treeview_Y.model().itemFromIndex(index).text() )
         
         
 if __name__ == '__main__' :
