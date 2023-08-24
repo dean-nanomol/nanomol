1a8992dab91822d8834a1d1240e37f3d1e04970e
dean-nanomol <135820523+dean-nanomol@users.noreply.github.com>
2023-08-24 18:24:43 +0200

    update transistor_output_transfer class

--- experiments/transistor_output_transfer.py
+++ experiments/transistor_output_transfer.py
@@ -5,31 +5,93 @@ Created on Tue Aug 22 12:02:09 2023
 @author: deankos
 """
 
+import numpy as np
+import threading
+import time
 import pyqtgraph as pg
 from PyQt5 import QtWidgets, uic
 from nanomol.instruments.keithley_2600A import keithley_2600A
-from nanomol.utils.experiment_ui import experiment_ui
+from nanomol.utils.interactive_ui import interactive_ui
 from nanomol.utils.hdf5_datafile import hdf5_datafile
 from nanomol.utils.hdf5_viewer import hdf5_viewer
 
-class transistor_output_transfer(experiment_ui):
+class transistor_output_transfer(interactive_ui):
     
-    def __init__(self, smu_address):
+    def __init__(self, datafile, smu_address):
         super().__init__()
+        self.datafile = datafile
         self.smu = keithley_2600A(smu_address)
         # set gate-source and drain-source channels
-        self.GS = 'a'
-        self.DS = 'b'
+        self.smu_channels = {'GS': 'a', 'DS': 'b'}
         uic.loadUi(r'transistor_output_transfer.ui', self)
         self.connect_widgets_by_name()
+        self.output_mode_radiobutton.clicked.connect(self.set_measurement_mode)
+        self.transfer_mode_radiobutton.clicked.connect(self.set_measurement_mode)
+        self.start_pushbutton.clicked.connect(self.start_measurement)
+        self.set_measurement_mode()
+        self.measurement_is_running = False
+    
+    def start_measurement(self):
+        if not self.measurement_is_running:  # do nothing if measurement is already running
+            self.measurement_is_running = True
+            measurement_thread = threading.Thread(target=self.run_measurement)
+            measurement_thread.start()
+            
+    def run_measurement(self):
+        self.active_group = self.datafile.create_group('test_group')
+        self.configure_measurement()
+        self.smu.set_output(self.V1_ch, 1)
+        self.smu.set_output(self.V2_ch, 1)
+        for self.active_V1 in self.V1:
+            self.smu.set_source_level(self.V1_ch, 'v', self.V1_active)
+            for self.active_V2 in self.V2:
+                self.smu.set_source_level(self.V2_ch, 'v', self.V2_active)
+                [self.measured_V1, self.measured_I1] = self.smu.measure(self.V1_ch, 'iv')
+                [self.measured_V2, self.measured_I2] = self.smu.measure(self.V2_ch, 'iv')
+                self.save_datapoint()
+        self.smu.set_output(self.V1_ch, 0)
+        self.smu.set_output(self.V2_ch, 0)
+        self.measurement_is_running = False
+        
+        
+    def configure_measurement(self):
+        self.attrs = {}
+        if self.measurement_mode == 'output':
+            self.V1_ch = self.smu_channels['GS']
+            self.V2_ch = self.smu_channels['DS']
+            self.V1 = np.arange(self.V_GS_min_output, self.V_GS_max_output + self.V_GS_step_output, self.V_GS_step_output)
+            self.V2 = np.arange(self.V_DS_min_output, self.V_DS_max_output + self.V_DS_step_output, self.V_DS_step_output)
+        elif self.measurement_mode == 'transfer':
+            self.V1_ch = self.smu_channels['DS']
+            self.V2_ch = self.smu_channels['GS']
+            self.V1 = np.arange(self.V_DS_min_transfer, self.DS_max_transfer + self.V_DS_step_transfer, self.V_DS_step_transfer)
+            self.V2 = np.arange(self.V_GS_min_transfer, self.GS_max_transfer + self.V_GS_step_transfer, self.V_GS_step_transfer)
+        # set channels to start from first point when output is turned on
+        self.V1_active = self.V1[0]
+        self.V2_active = self.V2[0]
+        self.smu.set_source_level(self.V1_ch, 'v', self.V1_active)
+        self.smu.set_source_level(self.V2_ch, 'v', self.V2_active)
+        
+    
+    def set_measurement_mode(self):
+        if self.output_mode_radiobutton.isChecked():
+            self.measurement_mode = 'output'
+        elif self.transfer_mode_radiobutton.isChecked():
+            self.measurement_mode = 'transfer'
+       
+    ### TO DO: append/create dataset; append() does not exist, maybe could just use create() at the end of V2 loop
+    ### save GS and DS channel info correctly regardless of measurment mode output/transfer
+    def save_datapoint(self):
+        self.active_group.append_dataset()
+            
         
 if __name__ == '__main__' :
     
-    #datafile = hdf5_datafile(mode='x')
+    datafile = hdf5_datafile(mode='x')
     
     experiment_app = QtWidgets.QApplication([])
-    #experiment = transistor_output_transfer(datafile, 'GPIB0::27::INSTR')
-    experiment = transistor_output_transfer('GPIB0::27::INSTR')
+    experiment = transistor_output_transfer(datafile, 'GPIB0::27::INSTR')
+    #experiment = transistor_output_transfer('GPIB0::27::INSTR')
     experiment.show()
     experiment_app.exec()
     
