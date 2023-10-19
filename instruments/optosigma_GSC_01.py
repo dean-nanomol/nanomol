b8f87c229f92cbf88535b2ba05f3b4436fc8adbd
dean-nanomol <135820523+dean-nanomol@users.noreply.github.com>
2023-10-19 18:56:14 +0200

    update serial_instrument, GSC01

--- instruments/optosigma_GSC_01.py
+++ instruments/optosigma_GSC_01.py
@@ -5,9 +5,42 @@ Created on Wed Oct 18 16:47:42 2023
 @author: deankos
 """
 
-import serial
+from nanomol.instruments.serial_instrument import serial_instrument
 
-class optosigma_GSC_01():
+class optosigma_GSC_01(serial_instrument):
     
-    def __init__(self, address=''):
-        self.reset()
+    def __init__(self, port):
+        settings = {'baudrate': 9600,
+                    'rtscts': True,
+                    'timeout': 1 }
+        termination = '\r\n'
+        super().__init__(port=port, port_settings=settings, termination_character=termination)
+    
+    def home(self):
+        command_state = self.query('H:1')
+        return command_state
+        
+    def move_relative(self, pulses):
+        if pulses < 0:
+            command_state = self.query('M:1-P{}'.format(int(abs(pulses)) ) )
+        elif pulses > 0:
+            command_state = self.query('M:1+P{}'.format(int(pulses)) )
+        if command_state == 'OK':
+            command_state = self.query('G:')
+        return command_state
+    
+    def position(self):
+        device_state = self.query('Q:')
+        position = device_state.split(',', 1)[0]
+        position = position.replace(' ', '')
+        return int(position)
+    
+    def state(self):
+        device_state = self.query('Q:')
+        device_state = device_state.split(',', 1)[1]
+        return device_state
+            
+        
+if __name__ == '__main__' :
+    
+    myGSC01 = optosigma_GSC_01('COM1')
--- instruments/serial_instrument.py
+++ instruments/serial_instrument.py
@@ -8,11 +8,49 @@ Created on Wed Oct 18 16:59:42 2023
 import serial
 
 class serial_instrument():
+    """
+    Wrapper for serial.Serial class of pyserial. Functions include binary conversions and termination
+    characters, so they directly accept and return python strings.
+    """
     
-    def __init__(self, port=None, port_settings={} ):
+    def __init__(self, port=None, port_settings={}, termination_character='\n'):
+        """
+        port : str
+            Communication port to open, for example 'COM1'
+        port_settings : dict
+            Dictionary of port settings. See pySerial API documentation for full parameter list.
+        termination_character : str
+            Termination character for all messages, usually '\n', '\r' or 'r\n'
+        """
+        
         self.instrument = serial.Serial(port, **port_settings)
+        self.termination_character = termination_character
+        
+    def write(self, command):
+        """ command : str """
+        binary_command = (command + self.termination_character).encode()
+        self.instrument.write(binary_command)
+        
+    def read(self):
+        """ read from buffer until next termination character """
+        message = self.instrument.read_until(expected = self.termination_character.encode() )
+        message = message.decode()
+        return message.rstrip()
+    
+    def query(self, command):
+        """ send command and return reply """
+        self.write(command)
+        return self.read()
+    
+    def close(self):
+        self.instrument.close()
+        
         
 if __name__ == '__main__' :
     
-    settings = {'baudrate': 9600}
-    mySerialInstrument = serial_instrument('COM1', settings)
+    settings = {'baudrate': 9600,
+                'rtscts': True,
+                'timeout': 5 }
+    termination = '\r\n'
+    
+    mySerialInstrument = serial_instrument(port='COM1', port_settings=settings, termination_character=termination)
