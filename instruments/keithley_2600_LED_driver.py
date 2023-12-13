6d4d1f022f16c596c37c5596777258ee2399885a
dean-nanomol <135820523+dean-nanomol@users.noreply.github.com>
2023-12-13 17:50:25 +0100

    update keithley LED driver

--- instruments/keithley_2600_LED_driver.py
+++ instruments/keithley_2600_LED_driver.py
@@ -5,10 +5,16 @@ Created on Wed Dec 13 12:39:43 2023
 @author: deankos
 """
 
+import os
+from PyQt5 import QtWidgets, uic
+from nanomol.instruments.visa_instrument import visa_instrument
 from nanomol.instruments.keithley_2600A import keithley_2600A
 
-class keithley_2600_LED_driver():
+class keithley_2600_LED_driver_ui():
     """ Keithley SMU used to drive an LED """
     
     def __init__(self, smu):
         super().__init__()
+        self.smu = smu
+        
+    
