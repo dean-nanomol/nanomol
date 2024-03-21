7a529a23e2e593b8159900b6728eea34385d84fc
dean-nanomol <135820523+dean-nanomol@users.noreply.github.com>
2024-03-21 19:44:41 +0100

    Update thorlabs_MCLS1.py

--- instruments/thorlabs_MCLS1.py
+++ instruments/thorlabs_MCLS1.py
@@ -19,6 +19,85 @@ class thorlabs_MCLS1(serial_instrument):
                     'timeout': 1 }
         termination = '\r'
         super().__init__(port=port, port_settings=settings, termination_character=termination)
+    
+    def query(self, command):
+        """ override serial_instrument.query method """
+        # MCLS1 repeats command text and adds '>', so buffer may not be empty
+        self.reset_input_buffer()
+        super().query(command) # ignore first returned string, contains command repetition
+        return self.read()
+    
+    @property
+    def channel(self):
+        return int(self.query('channel?'))
+    @channel.setter
+    def channel(self, channel):
+        """ active channel """
+        self.write('channel={}'.format(int(channel)))
+        
+    @property
+    def target_temperature(self):
+        return float(self.query('target?'))
+    @target_temperature.setter
+    def target_temperature(self, target_temperature):
+        """
+        target_temperature : float
+            active channel target temperature in °C, 20 <= target_temperature <= 30
+        """
+        self.write('target={}'.format(target_temperature))
+    
+    @property
+    def current(self):
+        return float(self.query('current?'))
+    @current.setter
+    def current(self, current):
+        """
+        current : float
+            active channel current in mA
+        """
+        self.write('current={}'.format(current))
+    
+    @property
+    def temperature(self):
+        """ active channel temperature in °C """
+        return float(self.query('temp?'))
+    
+    @property
+    def power(self):
+        """ active channel laser power in mW """
+        return float(self.query('power?'))
+    
+    @property
+    def enable(self):
+        return int(self.query('enable?'))
+    @enable.setter
+    def enable(self, enable):
+        """
+        enable : int
+            0 or 1, disable or enable active laser channel
+        """
+        self.write('enable={}'.format(enable))
+        
+    @property
+    def system_enable(self):
+        return int(self.query('system?'))
+    @system_enable.setter
+    def system_enable(self, system_enable):
+        """
+        system_enable : int
+            0 or 1, disable or enable laser system
+        """
+        self.write('system={}'.format(system_enable))
+        
+    @property
+    def specs(self):
+        """ active laser channel specifications """
+        return self.query('specs?')
+    
+    @property
+    def status(self):
+        """ status of all 'enable' buttons """
+        return self.query('statword?')
         
 if __name__ == '__main__' :
     
