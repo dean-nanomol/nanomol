07af2a7c28f072e2d1a3488f8abb9972b0ceebb5
dean-nanomol <135820523+dean-nanomol@users.noreply.github.com>
2024-03-25 13:25:11 +0100

    update MCLS1 class and ui

--- instruments/thorlabs_MCLS1.py
+++ instruments/thorlabs_MCLS1.py
@@ -111,15 +111,110 @@ class thorlabs_MCLS1_ui(interactive_ui):
         ui_file_path = os.path.join(os.path.dirname(__file__), 'throlabs_MCSL1.ui')
         uic.loadUi(ui_file_path, self)
         self.connect_widgets_by_name()
-        self.system_enable_checkBox.toggled.connect(self.system_enable)
+        self.system_enable_checkBox.stateChanged.connect(self.system_enable)
         
+        enable_checkBoxes = [self.enable_ch1_checkBox, self.enable_ch2_checkBox,
+                          self.enable_ch3_checkBox, self.enable_ch4_checkBox]
+        for checkBox in enable_checkBoxes:
+            checkBox.stateChanged.connect(self.enable)
+            
+        set_current_spinBoxes = [self.current_ch1_doubleSpinBox, self.current_ch2_doubleSpinBox,
+                               self.current_ch3_doubleSpinBox, self.current_ch4_doubleSpinBox]
+        for spinBox in set_current_spinBoxes:
+            spinBox.valueChanged.connect(self.set_current)
+            
+        get_power_buttons = [self.get_power_ch1_pushButton, self.get_power_ch2_pushButton,
+                             self.get_power_ch3_pushButton, self.get_power_ch4_pushButton]
+        for pushButton in get_power_buttons:
+            pushButton.clicked.connect(self.get_power)
+            
+        target_T_spinBoxes = [self.target_T_ch1_doubleSpinBox, self.target_T_ch2_doubleSpinBox,
+                              self.target_T_ch3_doubleSpinBox, self.target_T_ch4_doubleSpinBox]
+        for spinBox in target_T_spinBoxes:
+            spinBox.valueChanged.connect(self.set_target_temperature)
+            
+        get_T_pushButtons = [self.get_T_ch1_pushButton, self.get_T_ch2_pushButton,
+                             self.get_T_ch3_pushButton, self.get_T_ch4_pushButton]
+        for pushButton in get_T_pushButtons:
+            pushButton.clicked.connect(self.get_temperature)
         
     def system_enable(self):
         if self.system_enable_checkBox.isChecked():
             self.MCLS1.system_enable = 1
         else:
             self.MCLS1.system_enable = 0
+    
+    def enable(self):
+        if self.sender() == self.enable_ch1_checkBox:
+            self.MCLS1.channel = 1
+        elif self.sender() == self.enable_ch2_checkBox:
+            self.MCLS1.channel = 2
+        elif self.sender() == self.enable_ch3_checkBox:
+            self.MCLS1.channel = 3
+        elif self.sender() == self.enable_ch4_checkBox:
+            self.MCLS1.channel = 4
+        if self.sender().isChecked():
+            self.MCLS1.enable = 1
+        else:
+            self.MCLS1.enable = 0
 
+    def set_current(self):
+        if self.sender() == self.current_ch1_doubleSpinBox:
+            self.MCLS1.channel = 1
+            current = self.current_ch1
+        elif self.sender() == self.current_ch2_doubleSpinBox:
+            self.MCLS1.channel = 2
+            current = self.current_ch2
+        elif self.sender() == self.current_ch3_doubleSpinBox:
+            self.MCLS1.channel = 3
+            current = self.current_ch3
+        elif self.sender() == self.current_ch4_doubleSpinBox:
+            self.MCLS1.channel = 4
+            current = self.current_ch4
+        self.MCLS1.current = current
+        
+    def get_power(self):
+        if self.sender() == self.get_power_ch1_pushButton:
+            self.MCLS1.channel = 1
+            self.power_ch1_lineEdit.setText(self.MCLS1.power)
+        elif self.sender() == self.get_power_ch2_pushButton:
+            self.MCLS1.channel = 2
+            self.power_ch2_lineEdit.setText(self.MCLS1.power)
+        elif self.sender() == self.get_power_ch3_pushButton:
+            self.MCLS1.channel = 3
+            self.power_ch3_lineEdit.setText(self.MCLS1.power)
+        elif self.sender() == self.get_power_ch4_pushButton:
+            self.MCLS1.channel = 4
+            self.power_ch4_lineEdit.setText(self.MCLS1.power)
+    
+    def set_target_temperature(self):
+        if self.sender() == self.target_T_ch1_doubleSpinBox:
+            self.MCLS1.channel = 1
+            target_T = self.target_T_ch1
+        elif self.sender() == self.target_T_ch2_doubleSpinBox:
+            self.MCLS1.channel = 2
+            target_T = self.target_T_ch2
+        elif self.sender() == self.target_T_ch3_doubleSpinBox:
+            self.MCLS1.channel = 3
+            target_T = self.target_T_ch3
+        elif self.sender() == self.target_T_ch4_doubleSpinBox:
+            self.MCLS1.channel = 4
+            target_T = self.target_T_ch4
+        self.MCLS1.target_temperature = target_T
+    
+    def get_temperature(self):
+        if self.sender() == self.get_T_ch1_pushButton:
+            self.MCLS1.channel = 1
+            self.T_ch1_lineEdit.setText(self.MCLS1.temperature)
+        elif self.sender() == self.get_T_ch2_pushButton:
+            self.MCLS1.channel = 2
+            self.T_ch2_lineEdit.setText(self.MCLS1.temperature)
+        elif self.sender() == self.get_T_ch3_pushButton:
+            self.MCLS1.channel = 3
+            self.T_ch3_lineEdit.setText(self.MCLS1.temperature)
+        elif self.sender() == self.get_T_ch4_pushButton:
+            self.MCLS1.channel = 4
+            self.T_ch4_lineEdit.setText(self.MCLS1.temperature)
         
         
 if __name__ == '__main__' :
