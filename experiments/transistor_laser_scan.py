d517b114b0984ba1f8b120fa73dc311e3e6cf486
dean-nanomol <135820523+dean-nanomol@users.noreply.github.com>
2024-05-07 14:53:54 +0200

    Create transistor_laser_scan.py

---
+++ experiments/transistor_laser_scan.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  7 11:48:04 2024
+
+@author: deankos
+"""
+
+import numpy as np
+import threading
+import time
+import os
+import pyqtgraph as pg
+from PyQt5 import QtWidgets, uic
+from PyQt5.QtCore import pyqtSignal
+from nanomol.instruments.keithley_2600A import keithley_2600A, keithley_2600A_ui
+from nanomol.utils.interactive_ui import interactive_ui
+from nanomol.utils.hdf5_datafile import hdf5_datafile
+from nanomol.utils.hdf5_viewer import hdf5_viewer
+
+class transistor_laser_scan(interactive_ui):
+    
