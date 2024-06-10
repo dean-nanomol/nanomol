3d64bc65a0875145319025ac1db13002cd03cf4f
dean-nanomol <135820523+dean-nanomol@users.noreply.github.com>
2024-06-10 19:18:49 +0200

    Update transistor_laser_scan.py

--- experiments/transistor_laser_scan.py
+++ experiments/transistor_laser_scan.py
@@ -15,6 +15,7 @@ from PyQt5.QtCore import pyqtSignal
 from nanomol.instruments.keithley_2600A import keithley_2600A, keithley_2600A_ui
 from nanomol.instruments.newport_CONEX_MFA_CC import newport_CONEX_MFA_CC, newport_CONEX_MFA_CC_XY_ui
 from nanomol.instruments.optosigma_GSC_01 import optosigma_GSC_01, optosigma_GSC_01_ui
+from nanomol.instruments.thorlabs_MCLS1 import thorlabs_MCLS1, thorlabs_MCLS1_ui
 from nanomol.utils.interactive_ui import interactive_ui
 from nanomol.utils.hdf5_datafile import hdf5_datafile
 from nanomol.utils.hdf5_viewer import hdf5_viewer
@@ -22,10 +23,11 @@ from nanomol.experiments.transistor_transfer import transistor_transfer
 
 class transistor_laser_scan(interactive_ui):
     
-    def __init__(self, stage_X, stage_Y, datafile, transistor_transfer):
+    def __init__(self, stage_X, stage_Y, MCLS1, datafile, transistor_transfer ):
         super().__init__()
         self.stage_X = stage_X
         self.stage_Y = stage_Y
+        self.MCLS1 = MCLS1
         self.datafile = datafile
         self.transfer = transistor_transfer
         ui_file_path = os.path.join(os.path.dirname(__file__), 'transistor_laser_scan.ui')
@@ -67,7 +69,6 @@ class transistor_laser_scan(interactive_ui):
                 self.stage_X.move_absolute(self.position_X)
                 self.wait_for_motion_completed(self.stage_X)
                 self.measure_grid_point()
-                time.sleep(0.2)
                 if not self.grid_scan_is_running:
                     break
             if not self.grid_scan_is_running:
@@ -103,13 +104,34 @@ class transistor_laser_scan(interactive_ui):
     def measure_grid_point(self):
         point_label = 'point_X{:.3f}_Y{:.3f}'.format(self.position_X, self.position_Y)
         self.active_point_group = self.active_scan_group.create_group(point_label)
-        self.transfer.start_measurement(data_path=self.active_point_group)
+        self.laserON_group = self.active_point_group.create_group('laser_ON')
+        self.laserON_group.attrs.create('laser_ON', 1)
+        self.save_laser_attrs(self.laserON_group)
+        self.transfer.start_measurement(datafile=self.datafile, path=self.laserON_group.name)
         self.transfer.measurement_thread.join()
-        print('X: {}, Y: {}'.format(self.stage_X.position(), self.stage_Y.position()))
+        self.laserOFF_group = self.active_point_group.create_group('laser_OFF')
+        self.laserOFF_group.attrs.create('laser_ON', 0)
+        self.stage_X.move_absolute(self.grid_X_start -0.05)
+        self.wait_for_motion_completed(self.stage_X)
+        self.transfer.start_measurement(datafile=self.datafile, path=self.laserOFF_group.name)
+        self.transfer.measurement_thread.join()
+        self.stage_X.move_absolute(self.position_X)
+        self.wait_for_motion_completed(self.stage_X)
         
     def save_scan_attrs(self):
-        scan_label = self.datafile.get_unique_group_name(self.datafile, basename='scan', max_N=100)
+        scan_label = self.datafile.get_unique_group_name(self.datafile, basename='scan', max_N=99)
         self.active_scan_group = self.datafile.create_group(scan_label)
+    
+    def save_laser_attrs(self, data_group):
+        laser_attrs = {}
+        laser_attrs['enabled_channels'] = self.MCLS1.status
+        laser_attrs['active_channel'] = self.MCLS1.channel
+        laser_attrs['power'] = self.MCLS1.power
+        laser_attrs['current'] = self.MCLS1.current
+        laser_attrs['temperature'] = self.MCLS1.temperature
+        for attr, value in laser_attrs.items():
+            data_group.attrs.create(attr, value)
+        
     
     def wait_for_motion_completed(self, stage):
         while stage.is_moving():
@@ -123,6 +145,7 @@ if __name__ == '__main__' :
     stage_X = newport_CONEX_MFA_CC('COM5')
     stage_Y = newport_CONEX_MFA_CC('COM6')
     stage_Z = optosigma_GSC_01('COM1')
+    MCLS1 = thorlabs_MCLS1('COM4')
     
     ui_app = QtWidgets.QApplication([])
     
@@ -130,13 +153,15 @@ if __name__ == '__main__' :
     smu_ui = keithley_2600A_ui(smu)
     stage_XY_ui = newport_CONEX_MFA_CC_XY_ui(stage_X, stage_Y)
     stage_Z_ui = optosigma_GSC_01_ui(stage_Z)
-    transistor_transfer_ui = transistor_transfer(smu)
-    scan_ui = transistor_laser_scan(stage_X, stage_Y, datafile, transistor_transfer_ui)
+    MCLS1_ui = thorlabs_MCLS1_ui(MCLS1)
+    transistor_transfer_ui = transistor_transfer(smu, datafile)
+    scan_ui = transistor_laser_scan(stage_X, stage_Y, MCLS1, datafile, transistor_transfer_ui)
     
     datafile_viewer.show()
     smu_ui.show()
     stage_XY_ui.show()
     stage_Z_ui.show()
+    MCLS1_ui.show()
     transistor_transfer_ui.show()
     scan_ui.show()
     
